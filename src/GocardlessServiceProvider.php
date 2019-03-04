e3ce85c2a97df746e731d1870e5dcad58db30ab5
Eudald Arranz <eudald.arranz@nested.net>
2019-03-04 12:21:36 +0100

    Whops! Typo in the database migration fixed.

--- src/GocardlessServiceProvider.php
+++ src/GocardlessServiceProvider.php
@@ -38,7 +38,7 @@ class GocardlessServiceProvider extends ServiceProvider
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__.'/../../database/migrations/create_gocardless_webhook_calls_table.php.stub' => database_path('migrations/'.$timestamp.'_create_gocardless_webhook_calls_table.php'),
+                __DIR__.'/../database/migrations/create_gocardless_webhook_calls_table.php.stub' => database_path('migrations/'.$timestamp.'_create_gocardless_webhook_calls_table.php'),
             ], 'migrations');
         }
 
